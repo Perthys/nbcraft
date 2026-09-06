4b3491cf263071cf4360c06e4d03504acc8f8396
Perth <dragonwarrior1394@gmail.com>
2026-09-05 23:21:06 -0700

    Aligned spawn egg color channels

--- source/world/item/SpawnEggItem.cpp
+++ source/world/item/SpawnEggItem.cpp
@@ -32,18 +32,18 @@ const SpawnEggItem::Type* SpawnEggItem::Type::GetByEntityTypeID(EntityType::ID i
 
 void SpawnEggItem::initTypes()
 {
-	Type::_addEgg(EntityType::CREEPER,    Color::FromRGB(13, 167, 11), Color::FromRGB(0, 0, 0));
-	Type::_addEgg(EntityType::SKELETON,   Color::FromRGB(193, 193, 193), Color::FromRGB(73, 73, 73));
-	Type::_addEgg(EntityType::SPIDER,     Color::FromRGB(52, 45, 38), Color::FromRGB(168, 14, 14));
-	Type::_addEgg(EntityType::ZOMBIE,     Color::FromRGB(0, 175, 175), Color::FromRGB(121, 156, 101));
-	Type::_addEgg(EntityType::SLIME,      Color::FromRGB(81, 160, 62), Color::FromRGB(126, 191, 110));
+	Type::_addEgg(EntityType::CREEPER,    Color::FromRGB( 13, 167,  11), Color::FromRGB(  0,   0,   0));
+	Type::_addEgg(EntityType::SKELETON,   Color::FromRGB(193, 193, 193), Color::FromRGB( 73,  73,  73));
+	Type::_addEgg(EntityType::SPIDER,     Color::FromRGB( 52,  45,  38), Color::FromRGB(168,  14,  14));
+	Type::_addEgg(EntityType::ZOMBIE,     Color::FromRGB(  0, 175, 175), Color::FromRGB(121, 156, 101));
+	Type::_addEgg(EntityType::SLIME,      Color::FromRGB( 81, 160,  62), Color::FromRGB(126, 191, 110));
 	Type::_addEgg(EntityType::GHAST,      Color::FromRGB(249, 249, 249), Color::FromRGB(188, 188, 188));
-	Type::_addEgg(EntityType::PIG_ZOMBIE, Color::FromRGB(234, 147, 147), Color::FromRGB(76, 113, 41));
-	Type::_addEgg(EntityType::PIG,        Color::FromRGB(240, 165, 162), Color::FromRGB(219, 99, 95));
+	Type::_addEgg(EntityType::PIG_ZOMBIE, Color::FromRGB(234, 147, 147), Color::FromRGB( 76, 113,  41));
+	Type::_addEgg(EntityType::PIG,        Color::FromRGB(240, 165, 162), Color::FromRGB(219,  99,  95));
 	Type::_addEgg(EntityType::SHEEP,      Color::FromRGB(231, 231, 231), Color::FromRGB(255, 181, 181));
-	Type::_addEgg(EntityType::COW,        Color::FromRGB(68, 54, 38), Color::FromRGB(161, 161, 161));
-	Type::_addEgg(EntityType::CHICKEN,    Color::FromRGB(161, 161, 161), Color::FromRGB(255, 0, 0));
-	Type::_addEgg(EntityType::SQUID,      Color::FromRGB(34, 59, 77), Color::FromRGB(112, 136, 153));
+	Type::_addEgg(EntityType::COW,        Color::FromRGB( 68,  54,  38), Color::FromRGB(161, 161, 161));
+	Type::_addEgg(EntityType::CHICKEN,    Color::FromRGB(161, 161, 161), Color::FromRGB(255,   0,   0));
+	Type::_addEgg(EntityType::SQUID,      Color::FromRGB( 34,  59,  77), Color::FromRGB(112, 136, 153));
 }
 
 SpawnEggItem::SpawnEggItem(int itemID) : Item(itemID)
